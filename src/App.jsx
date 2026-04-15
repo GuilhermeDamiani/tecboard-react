092001cc9c86a8f2efa9732892e1527c913e7258
Guilherme Damiani <guilhermeborgesdamiani@gmail.com>
2026-04-14 21:07:28 -0300

    listando-eventos-e-imutabilidade

--- src/App.jsx
+++ src/App.jsx
@@ -45,18 +45,25 @@ function App() {
     }
   ]
 
+  function adicionarEvento(evento) {
+    eventos.push(evento)
+    console.log('eventos => ', eventos)
+  }
+
   return (
     <main>
       <header>
         <img src="/logo (2).png" alt="" />
       </header>
       <Banner />
-      <FormularioDeEvento temas={temas} />
+      <FormularioDeEvento temas={temas} aoSubmeter={adicionarEvento} />
       {temas.map(function (item) {
         return (
           <section key={item.id}>
             <Tema tema={item} />
-            <CardEvento evento={eventos[0]}/>
+            {eventos.map(function (item, index) {
+              return <CardEvento evento={item} key={index} />
+            })}
           </section>
         )
       })}
